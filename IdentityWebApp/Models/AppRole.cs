f34e5f0bf255b1e5bdc4c0007295e86a2fd6ec54
ogzhncengiz@outlook.com <ogzhncengiz@outlook.com>
2022-06-23 17:21:41 +0300

    Add project files.

---
+++ IdentityWebApp/Models/AppRole.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityWebApp.Models
+{
+    public class AppRole : IdentityRole
+    {
+        public string RoleType { get; set; }
+    }
+}
---
+++ IdentityWebApp/Models/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityWebApp.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string Picture { get; set; }
+        public string City { get; set; }
+    }
+}
