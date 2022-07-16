ccb99179b2a675685f907d9034e3223369501a3e
ogzhncengiz@outlook.com <ogzhncengiz@outlook.com>
2022-07-16 14:04:16 +0300

    16.07.2022

--- IdentityWebApp/Helpers/ResetPasswordHelper.cs
+++ IdentityWebApp/Helpers/ResetPasswordHelper.cs
@@ -9,16 +9,16 @@ namespace IdentityWebApp.Helpers
         {
             using (MailMessage mail = new MailMessage())
             {
-                mail.From = new MailAddress("x");
+                mail.From = new MailAddress("winston.reichel57@ethereal.email");
                 mail.To.Add(email);
                 mail.Subject = $"www.cengiz.com::Şifre yenileme";
                 mail.Body = "<h2>Şifrenizi yenilemek için lütfen aşagıdaki linke tıklayınız.</h2><hr/>";
                 mail.Body += $"<a href='{link}'>Şifre yenileme linki</a>";
                 mail.IsBodyHtml = true;
 
-                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                using (SmtpClient smtp = new SmtpClient("smtp.ethereal.email", 587))
                 {
-                    smtp.Credentials = new NetworkCredential("x", "x");
+                    smtp.Credentials = new NetworkCredential("winston.reichel57@ethereal.email", "hsSBaBh7J9vyQVSbNbw");
                     smtp.EnableSsl = true;
                     smtp.Send(mail);
                 }
