a0549057c113eb8df9c3e385874dec57f345ddad
ogzhncengiz@outlook.com <ogzhncengiz@outlook.com>
2022-07-16 15:54:03 +0300

    16.07.2022

--- IdentityWebApp/Controllers/MemberController.cs
+++ IdentityWebApp/Controllers/MemberController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using IdentityWebApp.Models;
+using IdentityWebApp.Models.ViewModels;
+using Mapster;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityWebApp.Controllers
@@ -6,9 +10,22 @@ namespace IdentityWebApp.Controllers
     [Authorize]
     public class MemberController : Controller
     {
+        private readonly ILogger<MemberController> _logger;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public MemberController(ILogger<MemberController> logger, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         public IActionResult Index()
         {
-            return View();
+            AppUser user = _userManager.FindByNameAsync(User.Identity.Name).Result;
+            UserViewModel userViewModel = user.Adapt<UserViewModel>();
+
+            return View(userViewModel);
         }
     }
 }
